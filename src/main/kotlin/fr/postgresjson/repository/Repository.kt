e819e1d027a0e697aa7f297bec3cfad286f9b08b
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2019-07-30 17:06:19 +0200

    clean repository

--- src/main/kotlin/fr/postgresjson/repository/Repository.kt
+++ src/main/kotlin/fr/postgresjson/repository/Repository.kt
@@ -1,30 +1,13 @@
 package fr.postgresjson.repository
 
 import fr.postgresjson.connexion.Requester
-import fr.postgresjson.entity.EntitiesCollections
 import fr.postgresjson.entity.EntityI
-import fr.postgresjson.serializer.Serializer
-import jdk.jfr.Experimental
 import kotlin.reflect.KClass
 
-interface RepositoryI<T, E: EntityI<T?>> {
+interface RepositoryI<E: EntityI<*>> {
     val entityName: KClass<E>
-}
-
-@Experimental
-abstract class Repository<T, E: EntityI<T?>>(override val entityName: KClass<E>): RepositoryI<T, E> {
-
-    abstract var requester: Requester
-    abstract fun getClassName(): String
-
-    fun <T> findById(id: T): EntityI<T?>? {
-        val sql = requester.getQuery(entityName.toString())
-        return when (val e = EntitiesCollections().get(id)) {
-            null -> {
-                // TODO create Request
-                Serializer().deserialize<T, EntityI<T?>>("""{"plop", "plip"}""")
-            }
-            else -> e
-        }
+    var requester: Requester
+    fun getClassName(): String {
+        return entityName.simpleName!!
     }
 }
