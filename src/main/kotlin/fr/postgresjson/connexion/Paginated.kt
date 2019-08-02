4fa8abea304d3c53fb0b3cb0e92d23859078ef89
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2019-08-03 00:54:10 +0200

    fix: pagination

--- src/main/kotlin/fr/postgresjson/connexion/Paginated.kt
+++ src/main/kotlin/fr/postgresjson/connexion/Paginated.kt
@@ -14,7 +14,7 @@ data class Paginated<T: EntityI<*>>(
 
     init {
         if (offset < 0) error("offset must be greather or equal than 0")
-        if (limit < 1) error("limit must be greather than 1")
-        if (total < 1) error("total must be greather or equal than 0")
+        if (limit < 1) error("limit must be greather or equal than 1")
+        if (total < 0) error("total must be greather or equal than 0")
     }
 }
