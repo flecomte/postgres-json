2f9f397f5047cedfe5972df69c43ef7b530a9414
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2019-08-30 15:14:54 +0200

    add EntityDeletedBy & EntityDeletedAt

--- src/main/kotlin/fr/postgresjson/entity/Entity.kt
+++ src/main/kotlin/fr/postgresjson/entity/Entity.kt
@@ -36,12 +36,24 @@ interface EntityUpdatedAt {
     var updatedAt: DateTime?
 }
 
+interface EntityDeletedAt {
+    var deletedAt: DateTime?
+    fun isDeleted(): Boolean {
+        val deletedAt = deletedAt
+        return deletedAt != null && deletedAt < DateTime.now()
+    }
+}
+
 class EntityCreatedAtImp: EntityCreatedAt {
     override var createdAt: DateTime? = null
 }
 
 class EntityUpdatedAtImp: EntityUpdatedAt {
     override var updatedAt: DateTime? = null
+}
+
+class EntityDeletedAtImp: EntityDeletedAt {
+    override var deletedAt: DateTime? = null
 }
 
 /* Author */
@@ -53,6 +65,10 @@ interface EntityUpdatedBy<T: EntityI<*>> {
     var updatedBy: T?
 }
 
+interface EntityDeletedBy<T: EntityI<*>> {
+    var deletedBy: T?
+}
+
 class EntityCreatedByImp<UserT: EntityI<*>>(
     override var createdBy: UserT?
 ): EntityCreatedBy<UserT>
@@ -60,6 +76,28 @@ class EntityCreatedByImp<UserT: EntityI<*>>(
 class EntityUpdatedByImp<UserT: EntityI<*>>(
     override var updatedBy: UserT?
 ): EntityUpdatedBy<UserT>
+
+class EntityDeletedByImp<UserT: EntityI<*>>(
+    override var deletedBy: UserT?
+): EntityDeletedBy<UserT>
+
+/* Mixed */
+class EntityDeletedImp<UserT: EntityI<*>>(
+    override var deletedBy: UserT? = null
+): EntityDeletedBy<UserT>,
+   EntityDeletedAt by EntityDeletedAtImp()
+
+class EntityUpdatedImp<UserT: EntityI<*>>(
+    override var updatedAt: DateTime? = null,
+    override var updatedBy: UserT? = null
+): EntityUpdatedBy<UserT>,
+   EntityUpdatedAt by EntityUpdatedAtImp()
+
+class EntityCreatedImp<UserT: EntityI<*>>(
+    override var createdAt: DateTime? = null,
+    override var createdBy: UserT? = null
+): EntityCreatedBy<UserT>,
+   EntityCreatedAt by EntityCreatedAtImp()
 
 /* Published */
 interface Published<UserT: EntityI<*>> {
@@ -79,8 +117,10 @@ abstract class EntityImp<T, UserT: EntityI<*>>(
 ): Entity<T>(),
     EntityCreatedAt by EntityCreatedAtImp(),
     EntityUpdatedAt by EntityUpdatedAtImp(),
+    EntityDeletedAt by EntityDeletedAtImp(),
     EntityCreatedBy<UserT> by EntityCreatedByImp(updatedBy),
-    EntityUpdatedBy<UserT> by EntityUpdatedByImp(updatedBy)
+    EntityUpdatedBy<UserT> by EntityUpdatedByImp(updatedBy),
+    EntityDeletedBy<UserT> by EntityDeletedByImp(updatedBy)
 
 abstract class UuidEntityExtended<T, UserT: EntityI<*>>(
     updatedBy: UserT?,
