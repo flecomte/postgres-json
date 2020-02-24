bbeec7bb604846c6b3ddd1315ed2393fef3f34a1
Fabrice Lecomte <fabrice.lecomte@non.se.com>
2020-02-24 20:41:29 +0100

    add methode$ "isLastPage" to pagination

--- src/main/kotlin/fr/postgresjson/connexion/Paginated.kt
+++ src/main/kotlin/fr/postgresjson/connexion/Paginated.kt
@@ -1,6 +1,7 @@
 package fr.postgresjson.connexion
 
 import fr.postgresjson.entity.EntityI
+import kotlin.math.ceil
 
 data class Paginated<T : EntityI>(
     val result: List<T>,
@@ -10,10 +11,15 @@ data class Paginated<T : EntityI>(
 ) {
     val currentPage: Int = (offset / limit) + 1
     val count: Int = result.size
+    val totalPages: Int = (total.toDouble() / limit.toDouble()).ceil()
 
     init {
         if (offset < 0) error("offset must be greather or equal than 0")
         if (limit < 1) error("limit must be greather or equal than 1")
         if (total < 0) error("total must be greather or equal than 0")
     }
+
+    fun isLastPage(): Boolean = currentPage >= totalPages
+
+    private fun Double.ceil(): Int = ceil(this).toInt()
 }
