a13ca2d954655cf47227e81469c9b2c20b9a70a6
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2021-02-26 23:00:41 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
     jacoco
 
     id("maven-publish")
-    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    kotlin("jvm") version "1.4.30"
 
-    id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
-    id("org.owasp.dependencycheck") version "5.1.0"
-    id("fr.coppernic.versioning") version "3.1.2"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.owasp.dependencycheck") version "6.1.1"
+    id("fr.coppernic.versioning") version "3.2.1"
 }
 
 group = "com.github.flecomte"
@@ -29,17 +29,17 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.31")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.9")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-joda:2.9.9")
-    implementation("com.github.jasync-sql:jasync-postgresql:1.0.7")
-    implementation("org.slf4j:slf4j-api:1.7.26")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.31")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-joda:2.12.1")
+    implementation("com.github.jasync-sql:jasync-postgresql:1.1.7")
+    implementation("org.slf4j:slf4j-api:1.7.30")
 
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
     testImplementation("ch.qos.logback:logback-core:1.2.3")
-    testImplementation("io.mockk:mockk:1.9")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    testImplementation("org.amshove.kluent:kluent:1.47")
+    testImplementation("io.mockk:mockk:1.10.6")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testImplementation("org.amshove.kluent:kluent:1.65")
 }
 
 val sourcesJar by tasks.creating(Jar::class) {
